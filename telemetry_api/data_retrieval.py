b67cbae984f7a132cd380f7f758317bcd1cda3cf
Brody Smith <bsmit272@uwo.ca>
2024-04-25 22:45:39 -0400

    Use query stream and buffered approach to CSV generation

--- telemetry_api/data_retrieval.py
+++ telemetry_api/data_retrieval.py
@@ -2,13 +2,14 @@ from influxdb_client import InfluxDBClient, Dialect
 import csv 
 import zipfile
 import os
+import mmap
 
 from motec_conversion.data_log import DataLog
 from motec_conversion.motec_log import MotecLog
 
 class InfluxDataRetrieval:
     def __init__(self, url, token, org):
-        self._influx_client = InfluxDBClient(url=url, token=token, org=org)
+        self._influx_client = InfluxDBClient(url=url, token=token, org=org, timeout=45000)
         self._query_api = self._influx_client.query_api()
 
     def _writeToCsv(self, device_name: str, header: list[str], rows: dict[str, list]) -> str:
@@ -41,55 +42,58 @@ class InfluxDataRetrieval:
             os.remove(file_name)
 
     # Return a tuple containing the last_time of measurement, and the file_names of all generated CSVs 
-    def _generateAllDataPointsCSV(self, tag) -> (str, list[str]):
+    def _generateAllDataPointsCSV(self, tag):
         # each MEASUREMENT (i.e., CAN device) will have an equal number of datapoints for all devices (CAN signals), 
         # because each datapoint represents a segment of a single CAN frame. On that CAN frame, there will always be values
         # for all other signals
-
-        device_signals = self._query_api.query(f'from(bucket: "RaceData") |> range(start: -6h) |> filter(fn: (r) => r.session_hash=="{tag}") |> group(columns: ["_measurement", "_field"])')
-        if len(device_signals) < 1:
-            # return an empty zip file when no data is present
-            return self._generateZip("no_data", []), []
-
-        this_device_name = device_signals[0].records[0].get_measurement()
-        this_signal_name = ""
-        header = ["timestamp"]
-        rows: dict[str, list] = {} # {"[timestamp]": [signal values]}
-        file_names = []
-
-        for device_signal in device_signals:
-            if device_signal.records[0].get_measurement() != this_device_name:
-                file_names.append(self._writeToCsv(this_device_name, header, rows))
-                header = ["timestamp"]
-                rows = {}        
-                this_device_name = device_signal.records[0].get_measurement()
-
-            time = ""
-            for signal_datapoint in device_signal.records:
-                if signal_datapoint.get_field() != this_signal_name:
-                    this_signal_name = signal_datapoint.get_field()
-                    header.append(this_signal_name)
-                
-                time = signal_datapoint.get_time()
-                if time in rows:
-                    rows[time].append(signal_datapoint.get_value())
+        f = open(f'static/data.csv', 'w', newline='')
+        records = self._query_api.query_stream(f'from(bucket: "RaceData") |> range(start: 0, stop: now()) |> filter(fn: (r) => r["session_hash"] == "{tag}") |> group(columns: ["_measurement"]) |> group(columns: ["_field"]) |> group(columns: ["_time"])')
+        current_col = 1
+        row = ""
+        table_no = -1
+        measurement_field_names_to_columns = {}
+        for record in records:
+            measurement_field_name = f'{record["_measurement"]}/{record["_field"]}'
+            if table_no != record["table"]: # then we know we are on a new set of signals 
+                if row != "":
+                    f.write(row + "\n")
+                    row = ""
+                table_no = record.table # a "table" is a set of signals from one message
+                row += f'{record["_time"]}' # change this into timestamp
+                if measurement_field_name in measurement_field_names_to_columns:
+                    current_col = 1
+                    row += ',' * measurement_field_names_to_columns[measurement_field_name] # add offset
+                    row += f'{record["_value"]}'
+                    current_col += 1
                 else:
-                    rows[time] = [signal_datapoint.get_value()]
+                    measurement_field_names_to_columns[measurement_field_name] = current_col
+                    row += ',' * current_col # add offset
+                    row += f'{record["_value"]}'
+                    current_col += 1
+            else:
+                if measurement_field_name in measurement_field_names_to_columns:
+                    row += f',{record["_value"]}'
+                else:
+                    measurement_field_names_to_columns[measurement_field_name] = current_col
+                    row += f',{record["_value"]}'
+                current_col += 1
         
-        file_names.append(self._writeToCsv(this_device_name, header, rows)) # handle the last device
-        return device_signals[0].records[0].get_stop(), file_names
+        f.close()
+        
+        # write the header in the beginning with memory mapping to avoid having to copy the entire file contents to memory 
+        with open("static/data.csv", "r+b") as f:
+            header = ("timestamp," + ','.join(list(measurement_field_names_to_columns.keys())) + "\n").encode("ascii")
+            mm = mmap.mmap(f.fileno(), 0)
+            existing = mm[:]
+            mm.resize(len(existing) + len(header))
+            mm[len(header):] = existing
+            mm[:len(header)] = header
+            mm.close()
+        return 'data.csv'
 
-    # Currently this is pretty slow when there's a lot of data. A method of increasing the speed is to 
-    # use multithreading: find out how many devices (measurements) there are first, spawn threads for each measurement,
-    # transform and write all points to a CSV for each signal on that device, have a running consumer thread that zips
-    # each file upon completion, close the zip after all CSV-writing threads finish, and return the name. Still probably will
-    # take a while, but less time. Maybe consider query optimization also so influx formats the data in the most congruent form for CSV
-    # writing, as I think most of the time comes from that processing portion.  
     def writeAllDataPointsWithTagCSV(self, tag) -> str:
-        last_time, file_names = self._generateAllDataPointsCSV(tag)
-        zip_name = self._generateZip(last_time, file_names)
-        self._deleteFiles(file_names)
-        return zip_name
+        name = self._generateAllDataPointsCSV(tag)
+        return name
 
     def writeAllDataPointsWithTagMotec(self, tag) -> str: 
         last_time, file_names = self._generateAllDataPointsCSV(tag)
